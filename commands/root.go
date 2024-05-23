835c250d3d3dcbe6948d11dbb9de4b20b66e9a8d
s1ntaxe770r <cyberdev01@protonmail.com>
2024-05-23 11:35:14 +0100

    chore: add inital cli scaffold

---
+++ commands/root.go
@@ -0,0 +1,31 @@
+package commands
+
+import (
+	"os"
+
+	"github.com/qernal/cli-qernal/commands/auth"
+	"github.com/spf13/cobra"
+)
+
+var RootCmd = &cobra.Command{
+	Use:   "qernal",
+	Short: "CLI for interacting with qernal.com",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) == 0 {
+			return cmd.Help()
+		}
+		return nil
+	},
+}
+
+func Execute() {
+	err := RootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	RootCmd.AddCommand(auth.AuthCmd)
+
+}
---
+++ config/config.go
@@ -0,0 +1,57 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/mitchellh/go-homedir"
+	"github.com/spf13/viper"
+)
+
+var (
+
+	// represents the current  config
+	Current Config
+)
+
+type ErrNoTokenFound struct{}
+
+func (m *ErrNoTokenFound) Error() string {
+	return "no token found"
+}
+
+type Config struct {
+	Token string `yaml:"token"`
+}
+
+func LoadConfig() {
+
+	token, found := os.LookupEnv("QERNAL_TOKEN")
+
+	if found && token != "" {
+		Current.Token = token
+		return
+	}
+
+	// lookup token in config
+	home, err := homedir.Dir()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	viper.SetConfigFile(fmt.Sprintf("%s/%s/%s", home, ".qernal", "config.yaml"))
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		fmt.Print(fmt.Errorf("unable to read qernal config", err).Error())
+	}
+
+	token = viper.Get("token").(string)
+	if Current.Token != "" {
+		Current.Token = token
+		return
+	}
+
+	// TODO: prompt user for token
+	fmt.Println("no token found")
+}
