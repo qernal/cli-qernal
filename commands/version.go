4b3ff54413e065b01a33117238b8855c3d22fab5
Jubril Oyetunji <cyberdev01@protonmail.com>
2024-11-26 16:08:48 -0500

    fix: default to verbose and support json

--- commands/version.go
+++ commands/version.go
@@ -5,29 +5,44 @@ import (
 	"runtime"
 
 	"github.com/qernal/cli-qernal/pkg/build"
+
+	"github.com/qernal/cli-qernal/pkg/common"
+	utils "github.com/qernal/cli-qernal/pkg/uitls"
 	"github.com/spf13/cobra"
 )
 
-var (
-	verbose bool
-)
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Output CLI version and build info",
-	Run: func(cmd *cobra.Command, args []string) {
-		switch {
-		case verbose:
-			fmt.Printf("Client version: %s\n", build.Version)
-			fmt.Printf("Build date (client): %s\n", build.Date)
-			fmt.Printf("Git commit (client): %s\n", build.Commit)
-			fmt.Printf("OS/Arch (client): %s/%s\n", runtime.GOOS, runtime.GOARCH)
-		default:
-			fmt.Printf("Qernal CLI %s\n", build.Version)
+	RunE: func(cmd *cobra.Command, args []string) error {
+
+		buildInfo := struct {
+			Version string `json:"version"`
+			Commit  string `json:"commit"`
+			Date    string `json:"date"`
+			OS      string `json:"os"`
+			Arch    string `json:"arch"`
+		}{
+			Version: build.Version,
+			Commit:  build.Commit,
+			Date:    build.Date,
+			OS:      runtime.GOOS,
+			Arch:    runtime.GOARCH,
 		}
+
+		if common.OutputFormat == "json" {
+			fmt.Println(utils.FormatOutput(buildInfo, common.OutputFormat))
+			return nil
+		}
+		fmt.Printf("Client version: %s\n", build.Version)
+		fmt.Printf("Build date (client): %s\n", build.Date)
+		fmt.Printf("Git commit (client): %s\n", build.Commit)
+		fmt.Printf("OS/Arch (client): %s/%s\n", runtime.GOOS, runtime.GOARCH)
+
+		return nil
 	},
 }
 
 func init() {
 	RootCmd.AddCommand(versionCmd)
-	versionCmd.Flags().BoolVarP(&verbose, "verbose", "v", false, "Use verbose output to see full information")
 }
