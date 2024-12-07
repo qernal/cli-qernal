26219b1078a6b8885b2f18dd53a2cd7c1d94367b
Jubril Oyetunji <cyberdev01@protonmail.com>
2024-12-07 14:27:51 -0500

    feat: introduce printer util

--- pkg/uitls/utils.go
+++ pkg/uitls/utils.go
@@ -1,8 +1,10 @@
 package utils
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/qernal/cli-qernal/charm"
@@ -35,4 +37,58 @@ func FormatOutput(data interface{}, outputType string) string {
 		}
 		return charm.PlainTextStyle.Render(fmt.Sprintf("%v", data))
 	}
+}
+
+type Printer struct {
+	resourceOut io.Writer
+}
+
+// NewPrinter creates a new Printer instance.
+func NewPrinter() *Printer {
+	return &Printer{
+		resourceOut: os.Stdout, // Default output to stdout
+	}
+}
+
+// SetResourceOutput sets the output for printing resources.
+func (p *Printer) SetOut(out io.Writer) {
+	p.resourceOut = out
+}
+
+// FormatOutput formats data based on the output type
+func (p *Printer) FormatOutput(data interface{}, outputType string) string {
+	var out io.Writer = os.Stdout
+	if p.resourceOut != nil {
+		out = p.resourceOut
+	}
+
+	switch outputType {
+	case "json":
+		prettyJSON, err := PrettyPrintJSON(data)
+		if err != nil {
+			fmt.Fprintln(out, "invalid json data")
+			os.Exit(1)
+		}
+		return prettyJSON
+	default:
+		if mapData, ok := data.(map[string]interface{}); ok {
+			formattedMap := new(bytes.Buffer)
+			for key, value := range mapData {
+				fmt.Fprintf(formattedMap, "%s: %v\n", key, value)
+			}
+			fmt.Fprint(out, formattedMap.String())
+			return formattedMap.String()
+		}
+		formattedString := fmt.Sprintf("%v", data)
+		return formattedString
+	}
+}
+
+// PrintResource directly prints the given data to the output.
+func (p *Printer) PrintResource(data string) {
+	var out io.Writer = os.Stdout
+	if p.resourceOut != nil {
+		out = p.resourceOut
+	}
+	fmt.Fprintln(out, data)
 }
