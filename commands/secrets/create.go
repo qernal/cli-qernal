e662e62a51157fbb7b4808e25f12fb630a52037b
Jubril Oyetunji <cyberdev01@protonmail.com>
2024-12-21 02:16:07 -0500

    feat: refactor to use printer utility

--- commands/secrets/create.go
+++ commands/secrets/create.go
@@ -5,15 +5,15 @@ import (
 	"context"
 	"fmt"
 	"io/ioutil"
-	"os"
 	"strings"
 
 	"github.com/qernal/cli-qernal/charm"
 	"github.com/qernal/cli-qernal/commands/auth"
 	"github.com/qernal/cli-qernal/pkg/client"
+	"github.com/qernal/cli-qernal/pkg/common"
+	"github.com/qernal/cli-qernal/pkg/utils"
 	openapi_chaos_client "github.com/qernal/openapi-chaos-go-client"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 var secretType string
@@ -23,14 +23,15 @@ var privateKey string
 
 // var environmentValue string
 
-var CreateCmd = &cobra.Command{
-	Use:     "create",
-	Aliases: []string{"new"},
-	Short:   "Create a new secret",
-	Long: `Create a new secret in your Qernal project. 
+func NewCreateCmd(printer *utils.Printer) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "create",
+		Aliases: []string{"new"},
+		Short:   "Create a new secret",
+		Long: `Create a new secret in your Qernal project. 
 This command supports creating registry, environment, and certificate secrets.
 The secret value is read from stdin, allowing for secure input methods.`,
-	Example: ` # Create a registry secret
+		Example: ` # Create a registry secret
   echo <registry-password> | qernal secret create --name MyRegistrySecret --type registry --registry-url docker.io
 
   # Create an environment secret
@@ -38,133 +39,146 @@ The secret value is read from stdin, allowing for secure input methods.`,
 
   # Create a certificate secret
   qernal secret create --name MyCertSecret --type certificate --public-key /path/to/public.key --private-key /path/to/private.key`,
-	RunE: func(cmd *cobra.Command, args []string) error {
-		if len(args) > 0 {
-			return charm.RenderError("No arguments expected. Please provide input through stdin.")
-		}
-		// Read from stdin
-		reader := bufio.NewReader(os.Stdin)
-		plaintext, err := reader.ReadString('\n')
-		if err != nil {
-			return charm.RenderError("Error reading input from stdin:", err)
-		}
-		// Remove trailing newline from input
-		plaintext = strings.TrimSpace(plaintext)
-		ctx := context.Background()
+		RunE: func(cmd *cobra.Command, args []string) error {
 
-		token, err := auth.GetQernalToken()
-
-		if err != nil {
-			return charm.RenderError("unable to retreive qernal token, run qernal auth login if you haven't")
-
-		}
-		qc, err := client.New(ctx, nil, nil, token)
-		if err != nil {
-			return charm.RenderError("", err)
-		}
-
-		dek, err := qc.FetchDek(ctx, projectID)
-		if err != nil {
-			return charm.RenderError("unable to fetch dek key", err)
-		}
-		switch secretType {
-		case "registry":
-			// mark registry flag required
-			if registry == "" {
-				return charm.RenderError("--registry is required to create a registry secret")
+			if len(args) > 0 && secretType != "certificate" {
+				return charm.RenderError("No arguments expected. Please provide input through stdin.")
 			}
-			encryptedValue, err := client.EncryptLocalSecret(dek.Payload.SecretMetaResponseDek.Public, plaintext)
+
+			ctx := context.Background()
+
+			token, err := auth.GetQernalToken()
+
 			if err != nil {
-				charm.RenderError("unable to  encrypt input", err)
+				return charm.RenderError("unable to retrieve qernal token, run qernal auth login if you haven't")
 
 			}
-			encryptionRef := fmt.Sprintf(`keys/dek/%d`, dek.Revision)
-			_, _, err = qc.SecretsAPI.ProjectsSecretsCreate(ctx, projectID).SecretBody(openapi_chaos_client.SecretBody{
-				Name:       strings.ToUpper(secretName),
-				Encryption: encryptionRef,
-				Type:       openapi_chaos_client.SECRETCREATETYPE_REGISTRY,
-				Payload: openapi_chaos_client.SecretCreatePayload{
-					SecretRegistry: &openapi_chaos_client.SecretRegistry{
-						Registry:      registry,
-						RegistryValue: encryptedValue,
+			qc, err := client.New(ctx, nil, nil, token)
+			if err != nil {
+				return charm.RenderError("", err)
+			}
+
+			dek, err := qc.FetchDek(ctx, projectID)
+			if err != nil {
+				return charm.RenderError("unable to fetch dek key", err)
+			}
+			switch secretType {
+			case "registry":
+				// mark registry flag required
+				if registry == "" {
+					return charm.RenderError("--registry is required to create a registry secret")
+				}
+				// Read from stdin
+				reader := bufio.NewReader(cmd.InOrStdin())
+				plaintext, err := reader.ReadString('\n')
+				if err != nil {
+					return charm.RenderError("Error reading input from stdin:", err)
+				}
+				// Remove trailing newline from input
+				plaintext = strings.TrimSpace(plaintext)
+				encryptedValue, err := client.EncryptLocalSecret(dek.Payload.SecretMetaResponseDek.Public, plaintext)
+				if err != nil {
+					return charm.RenderError("unable to  encrypt input", err)
+
+				}
+				encryptionRef := fmt.Sprintf(`keys/dek/%d`, dek.Revision)
+				_, _, err = qc.SecretsAPI.ProjectsSecretsCreate(ctx, projectID).SecretBody(openapi_chaos_client.SecretBody{
+					Name:       strings.ToUpper(secretName),
+					Encryption: encryptionRef,
+					Type:       openapi_chaos_client.SECRETCREATETYPE_REGISTRY,
+					Payload: openapi_chaos_client.SecretCreatePayload{
+						SecretRegistry: &openapi_chaos_client.SecretRegistry{
+							Registry:      registry,
+							RegistryValue: encryptedValue,
+						},
 					},
-				},
-			}).Execute()
-			if err != nil {
-				charm.RenderError("unable to  create registry secret", err)
+				}).Execute()
+				if err != nil {
+					return charm.RenderError("unable to  create registry secret", err)
 
+				}
+				printer.PrintResource(charm.SuccessStyle.Render("created registry secret with name " + secretName))
+				return nil
+			case "environment":
+				// Read from stdin
+				reader := bufio.NewReader(cmd.InOrStdin())
+				plaintext, err := reader.ReadString('\n')
+				if err != nil {
+					return charm.RenderError("Error reading input from stdin:", err)
+				}
+				// Remove trailing newline from input
+				plaintext = strings.TrimSpace(plaintext)
+				encryptedValue, err := client.EncryptLocalSecret(dek.Payload.SecretMetaResponseDek.Public, plaintext)
+				if err != nil {
+					return charm.RenderError("unable to  encrypt input", err)
+
+				}
+				encryptionRef := fmt.Sprintf(`keys/dek/%d`, dek.Revision)
+				_, _, err = qc.SecretsAPI.ProjectsSecretsCreate(ctx, projectID).SecretBody(openapi_chaos_client.SecretBody{
+					Name:       strings.ToUpper(secretName),
+					Encryption: encryptionRef,
+					Type:       openapi_chaos_client.SECRETCREATETYPE_ENVIRONMENT,
+					Payload: openapi_chaos_client.SecretCreatePayload{
+						SecretEnvironment: &openapi_chaos_client.SecretEnvironment{
+							EnvironmentValue: encryptedValue,
+						},
+					},
+				}).Execute()
+				if err != nil {
+					return charm.RenderError("unable to create environment secret", err)
+				}
+				printer.PrintResource(charm.SuccessStyle.Render("created environment secret with name " + secretName))
+			case "certificate":
+
+				if publicKey == "" || privateKey == "" {
+					return charm.RenderError("Both --public-key and --private-key flags must be provided", nil)
+				}
+
+				publicKeyContent, err := ioutil.ReadFile(publicKey)
+				if err != nil {
+					return charm.RenderError("Unable to read public key file", err)
+				}
+
+				privateKeyContent, err := ioutil.ReadFile(privateKey)
+				if err != nil {
+					return charm.RenderError("Unable to read private key file", err)
+				}
+
+				encryptionRef := fmt.Sprintf(`keys/dek/%d`, dek.Revision)
+				_, _, err = qc.SecretsAPI.ProjectsSecretsCreate(ctx, projectID).SecretBody(openapi_chaos_client.SecretBody{
+					Name:       strings.ToUpper(secretName),
+					Encryption: encryptionRef,
+					Type:       openapi_chaos_client.SECRETCREATETYPE_CERTIFICATE,
+					Payload: openapi_chaos_client.SecretCreatePayload{
+						SecretCertificate: &openapi_chaos_client.SecretCertificate{
+							Certificate:      strings.TrimSpace(string(publicKeyContent)),
+							CertificateValue: strings.TrimSpace(string(privateKeyContent)),
+						},
+					},
+				}).Execute()
+				if err != nil {
+					return charm.RenderError("Unable to create certificate secret", err)
+				}
+				printer.PrintResource(charm.SuccessStyle.Render("Created certificate secret with name " + secretName))
+				return nil
+			default:
+				return charm.RenderError("Invalid secret type. Must be on of 'registry', 'environment', or 'certificate'")
 			}
-			fmt.Println(charm.SuccessStyle.Render("created registry secret with name " + secretName))
 			return nil
-		case "environment":
-			encryptedValue, err := client.EncryptLocalSecret(dek.Payload.SecretMetaResponseDek.Public, plaintext)
-			if err != nil {
-				charm.RenderError("unable to  encrypt input", err)
+		},
+	}
 
-			}
-			encryptionRef := fmt.Sprintf(`keys/dek/%d`, dek.Revision)
-			_, _, err = qc.SecretsAPI.ProjectsSecretsCreate(ctx, projectID).SecretBody(openapi_chaos_client.SecretBody{
-				Name:       strings.ToUpper(secretName),
-				Encryption: encryptionRef,
-				Type:       openapi_chaos_client.SECRETCREATETYPE_ENVIRONMENT,
-				Payload: openapi_chaos_client.SecretCreatePayload{
-					SecretEnvironment: &openapi_chaos_client.SecretEnvironment{
-						EnvironmentValue: encryptedValue,
-					},
-				},
-			}).Execute()
-			if err != nil {
-				charm.RenderError("unable to create envrionment secret", err)
+	cmd.MarkFlagRequired("name")
+	cmd.MarkFlagRequired("project")
+	cmd.Flags().StringVarP(&secretType, "type", "t", "", "type of secret to be created (registry, environment, certificate")
+	cmd.Flags().StringVarP(&registry, "registry-url", "r", "", "Url to private container repository (for docker registry use docker.io)")
 
-			}
-		case "certificate":
-			if publicKey == "" || privateKey == "" {
-				return charm.RenderError("Both --public-key and --private-key are required for certificate type")
-			}
+	cmd.Flags().StringVarP(&secretName, "name", "n", "", "name of the secret")
+	cmd.Flags().StringVarP(&projectID, "project", "p", "", "ID of the project")
+	cmd.Flags().StringVarP(&common.OutputFormat, "output", "o", "text", "output format (json,text)")
 
-			publicKeyContent, err := ioutil.ReadFile(publicKey)
-			if err != nil {
-				return charm.RenderError("Unable to read public key file", err)
-			}
+	cmd.Flags().StringVarP(&publicKey, "public-key", "", "", "File path to the public key for certificate type")
+	cmd.Flags().StringVarP(&privateKey, "private-key", "", "", "File path to the private key for certificate type")
 
-			privateKeyContent, err := ioutil.ReadFile(privateKey)
-			if err != nil {
-				return charm.RenderError("Unable to read private key file", err)
-			}
-
-			encryptionRef := fmt.Sprintf(`keys/dek/%d`, dek.Revision)
-			_, _, err = qc.SecretsAPI.ProjectsSecretsCreate(ctx, projectID).SecretBody(openapi_chaos_client.SecretBody{
-				Name:       strings.ToUpper(secretName),
-				Encryption: encryptionRef,
-				Type:       openapi_chaos_client.SECRETCREATETYPE_CERTIFICATE,
-				Payload: openapi_chaos_client.SecretCreatePayload{
-					SecretCertificate: &openapi_chaos_client.SecretCertificate{
-						Certificate:      strings.TrimSpace(string(publicKeyContent)),
-						CertificateValue: strings.TrimSpace(string(privateKeyContent)),
-					},
-				},
-			}).Execute()
-			if err != nil {
-				return charm.RenderError("Unable to create certificate secret", err)
-			}
-			fmt.Println(charm.SuccessStyle.Render("Created certificate secret with name " + secretName))
-			return nil
-		default:
-			return charm.RenderError("Invalid secret type. Must be on of 'registry', 'environment', or 'certificate'")
-		}
-		return nil
-	},
-}
-
-func init() {
-	CreateCmd.MarkFlagRequired("name")
-	CreateCmd.MarkFlagRequired("project")
-	CreateCmd.Flags().StringVarP(&secretType, "type", "t", "", "type of secret to be created (registry, environment, certificate")
-	CreateCmd.Flags().StringVarP(&registry, "registry-url", "r", "", "Url to private container repository (for docker registry use docker.io)")
-
-	CreateCmd.Flags().StringVar(&publicKey, "public-key", "", "File path to the public key for certificate type")
-	CreateCmd.Flags().StringVar(&privateKey, "private-key", "", "File path to the private key for certificate type")
-
-	viper.BindPFlag("public-key", CreateCmd.Flags().Lookup("public-key"))
-	viper.BindPFlag("private-key", CreateCmd.Flags().Lookup("private-key"))
+	return cmd
 }
--- commands/secrets/encrypt.go
+++ commands/secrets/encrypt.go
@@ -3,8 +3,6 @@ package secrets
 import (
 	"bufio"
 	"context"
-	"fmt"
-	"os"
 	"strings"
 
 	"github.com/qernal/cli-qernal/charm"
@@ -15,71 +13,75 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var EncryptCmd = &cobra.Command{
-	Use:     "encrypt",
-	Short:   "encrypt a secret from stdin",
-	Example: "qernal encrypt <plaintext>",
-	Aliases: []string{
-		"e",
-	},
-	RunE: func(cmd *cobra.Command, args []string) error {
-		if len(args) > 0 {
-			return charm.RenderError("No arguments expected. Please provide input through stdin.")
-		}
+func NewEncryptCmd(printer *utils.Printer) *cobra.Command {
 
-		// Read from stdin
-		reader := bufio.NewReader(os.Stdin)
-		plaintext, err := reader.ReadString('\n')
-		if err != nil {
-			return charm.RenderError("Error reading input from stdin:", err)
-		}
-		// Remove trailing newline from input
-		plaintext = strings.TrimSpace(plaintext)
-		ctx := context.Background()
-
-		token, err := auth.GetQernalToken()
-
-		if err != nil {
-			return charm.RenderError("unable to retreive qernal token, run qernal auth login if you haven't")
-
-		}
-
-		qc, err := client.New(ctx, nil, nil, token)
-
-		if err != nil {
-			return charm.RenderError("", err)
-		}
-
-		dek, err := qc.FetchDek(ctx, projectID)
-		if err != nil {
-			return charm.RenderError("unable to fetch dek key", err)
-		}
-
-		encryptedVal, err := client.EncryptLocalSecret(dek.Payload.SecretMetaResponseDek.Public, plaintext)
-		if err != nil {
-			charm.RenderError("unable to  encrypt input", err)
-
-		}
-
-		var data interface{}
-		if common.OutputFormat == "json" {
-			data = map[string]interface{}{
-				"encrypted_value": encryptedVal,
-				"revision_id":     dek.Revision,
+	cmd := &cobra.Command{
+		Use:     "encrypt",
+		Short:   "encrypt a secret from stdin",
+		Example: "qernal encrypt <plaintext>",
+		Aliases: []string{
+			"e",
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if len(args) > 0 {
+				return charm.RenderError("No arguments expected. Please provide input through stdin.")
 			}
-		} else {
-			data = map[string]interface{}{
-				"Encrypted Value": encryptedVal,
-				"Revision ID":     dek.Revision,
+
+			// Read from stdin
+			reader := bufio.NewReader(cmd.InOrStdin())
+			plaintext, err := reader.ReadString('\n')
+			if err != nil {
+				return charm.RenderError("Error reading input from stdin:", err)
 			}
-		}
+			// Remove trailing newline from input
+			plaintext = strings.TrimSpace(plaintext)
+			ctx := context.Background()
 
-		fmt.Println(utils.FormatOutput(data, common.OutputFormat))
+			token, err := auth.GetQernalToken()
 
-		return nil
-	},
-}
+			if err != nil {
+				return charm.RenderError("unable to retreive qernal token, run qernal auth login if you haven't")
 
-func init() {
-	EncryptCmd.MarkFlagRequired("project")
+			}
+
+			qc, err := client.New(ctx, nil, nil, token)
+
+			if err != nil {
+				return charm.RenderError("", err)
+			}
+
+			dek, err := qc.FetchDek(ctx, projectID)
+			if err != nil {
+				return charm.RenderError("unable to fetch dek key", err)
+			}
+
+			encryptedVal, err := client.EncryptLocalSecret(dek.Payload.SecretMetaResponseDek.Public, plaintext)
+			if err != nil {
+				return charm.RenderError("unable to  encrypt input", err)
+
+			}
+
+			var data interface{}
+			if common.OutputFormat == "json" {
+				data = map[string]interface{}{
+					"encrypted_value": encryptedVal,
+					"revision_id":     dek.Revision,
+				}
+			} else {
+				data = map[string]interface{}{
+					"Encrypted Value": encryptedVal,
+					"Revision ID":     dek.Revision,
+				}
+			}
+
+			printer.PrintResource(utils.FormatOutput(data, common.OutputFormat))
+
+			return nil
+		},
+	}
+	cmd.Flags().StringVarP(&projectID, "project", "p", "", "ID of the project")
+	cmd.Flags().StringVarP(&common.OutputFormat, "output", "o", "text", "output format (json,text)")
+
+	cmd.MarkFlagRequired("project")
+	return cmd
 }
