0fc8afff737516bc243d33d6a85d53649f45440b
Jubril Oyetunji <cyberdev01@protonmail.com>
2025-01-24 18:14:34 +0100

    feat: org pagination

--- pkg/helpers/helpers.go
+++ pkg/helpers/helpers.go
@@ -259,7 +259,6 @@ func RandomSecretName() string {
 func PaginateProjects(printer *utils.Printer, ctx context.Context, qc *client.QernalAPIClient, maxResults int32) ([]openapi_chaos_client.ProjectResponse, error) {
 
 	initialResp, httpRes, err := qc.ProjectsAPI.ProjectsList(ctx).Execute()
-
 	if err != nil {
 		resData, _ := client.ParseResponseData(httpRes)
 		if data, ok := resData.(map[string]interface{}); ok {
@@ -274,6 +273,7 @@ func PaginateProjects(printer *utils.Printer, ctx context.Context, qc *client.Qe
 			slog.Any("response", resData))
 		return nil, printer.RenderError("unable to list projects", err)
 	}
+
 	allProjects := initialResp.GetData()
 	if initialResp.Meta.Results <= 20 {
 		return allProjects, nil
@@ -289,6 +289,69 @@ func PaginateProjects(printer *utils.Printer, ctx context.Context, qc *client.Qe
 		currentPage++
 		previous := currentPage - 1
 		next, httpRes, err := qc.ProjectsAPI.ProjectsList(ctx).
+			Page(openapi_chaos_client.OrganisationsListPageParameter{
+				Size:   &pageSize,
+				Before: &previous,
+				After:  &currentPage,
+			}).Execute()
+		if err != nil {
+			resData, _ := client.ParseResponseData(httpRes)
+			if data, ok := resData.(map[string]interface{}); ok {
+				if innerData, ok := data["data"].(map[string]interface{}); ok {
+					if nameErr, ok := innerData["name"].(string); ok {
+						return nil, printer.RenderError("unable to list projects", errors.New(nameErr))
+					}
+				}
+			}
+			printer.Logger.Debug("unable to list projects, request failed",
+				slog.String("error", err.Error()),
+				slog.Any("response", resData))
+			return nil, printer.RenderError("unable to list projects", err)
+		}
+
+		allProjects = append(allProjects, next.GetData()...)
+	}
+
+	if maxResults > 0 && len(allProjects) > int(maxResults) {
+		allProjects = allProjects[:maxResults]
+	}
+
+	return allProjects, nil
+}
+
+func PaginateOrganisations(printer *utils.Printer, ctx context.Context, qc *client.QernalAPIClient, maxResults int32) ([]openapi_chaos_client.OrganisationResponse, error) {
+
+	initialResp, httpRes, err := qc.OrganisationsAPI.OrganisationsList(ctx).Execute()
+	if err != nil {
+		resData, _ := client.ParseResponseData(httpRes)
+		if data, ok := resData.(map[string]interface{}); ok {
+			if innerData, ok := data["data"].(map[string]interface{}); ok {
+				if nameErr, ok := innerData["name"].(string); ok {
+					return nil, printer.RenderError("unable to list organisations", errors.New(nameErr))
+				}
+			}
+		}
+		printer.Logger.Debug("unable to list organisations, request failed",
+			slog.String("error", err.Error()),
+			slog.Any("response", resData))
+		return nil, printer.RenderError("unable to list organisations", err)
+	}
+
+	allProjects := initialResp.GetData()
+	if initialResp.Meta.Results <= 20 {
+		return allProjects, nil
+	}
+
+	pageSize := int32(20)
+	var currentPage int32
+	for currentPage < initialResp.Meta.Pages {
+		if maxResults > 0 && len(allProjects) >= int(maxResults) {
+			break
+		}
+
+		currentPage++
+		previous := currentPage - 1
+		next, httpRes, err := qc.OrganisationsAPI.OrganisationsList(ctx).
 			Page(openapi_chaos_client.OrganisationsListPageParameter{
 				Size:   &pageSize,
 				Before: &previous,
