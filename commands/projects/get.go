da9434c6cb364802fe43cbbb6e49ba304258d705
s1ntaxe770r <53065463+s1ntaxe770r@users.noreply.github.com>
2025-01-30 16:03:41 +0100

    refactor: cleanup and linting fixes (#40)
    * fix: update client to match provider func signature
    
    * fix: use new client singnature
    
    * feat: init secret tests
    
    * feat: refactor to use printer utility
    
    * feat: add cert and secret helpers
    
    * feat: standardise chaos client parameters (#33)
    
    * fix: update client to match provider func signature
    
    * fix: use new client singnature
    
    * fix: use int in json struct field
    
    * fix: clean up org only
    
    * feat: project delete test
    
    * feat: add helpers from terraform pkg
    
    * fix: use CreateSecretEnv
    
    * fix: use uppercase letters
    
    * feat: add configurable log level
    
    * feat: org commands
    
    * chore: formtatting
    
    * fix: suppress output
    
    * fix: resolved panic and orgs can be deleted by name
    
    * feat: find org by name
    
    * fix: updated delete flag
    
    * fix: update test
    
    * fix: render json errors properly
    
    * feat: add get org command
    
    * feat: project pagination
    
    * feat: org pagination
    
    * feat: secrets pagination
    
    * fix: broken test
    
    * ci: please errcheck
    
    * ci: please errcheck
    
    * ci: please errcheck
    
    * fix: broken test and errcheck
    
    * fix: broken test and errcheck
    
    * fix: broken test and errcheck
    
    * fix: golint
    
    * fix: golint
    
    * fix: golint
    
    * fix: broken test
    
    * ci: please errcheck
    
    * ci: please errcheck
    
    * ci: please errcheck
    
    * fix: broken test and errcheck
    
    * fix: broken test and errcheck
    
    * fix: broken test and errcheck
    
    * fix: golint
    
    * fix: golint
    
    * feat: project pagination
    
    * fix: remove unused var
    
    * feat: add get command for secrets and projects

---
+++ commands/projects/get.go
@@ -0,0 +1,69 @@
+package projects
+
+import (
+	"context"
+
+	"github.com/qernal/cli-qernal/charm"
+	"github.com/qernal/cli-qernal/commands/auth"
+	"github.com/qernal/cli-qernal/pkg/client"
+	"github.com/qernal/cli-qernal/pkg/common"
+	"github.com/qernal/cli-qernal/pkg/utils"
+	"github.com/spf13/cobra"
+)
+
+func NewGetCmd(printer *utils.Printer) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "get",
+		Aliases: []string{"get"},
+		Example: "qernal project get --name <org name>",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if len(args) > 0 {
+				return charm.RenderError("No arguments expected")
+			}
+
+			ctx := context.Background()
+			token, err := auth.GetQernalToken()
+			if err != nil {
+				return charm.RenderError("unable to retreive qernal token, run qernal auth login if you haven't")
+
+			}
+
+			qc, err := client.New(ctx, nil, nil, token)
+			if err != nil {
+				return charm.RenderError("error creating qernal client", err)
+			}
+
+			name, _ := cmd.Flags().GetString("name")
+
+			project, err := qc.GetProjectByName(name)
+			if err != nil {
+				return charm.RenderError("", err)
+			}
+
+			var data interface{}
+
+			if common.OutputFormat == "json" {
+				data = project
+			} else {
+				data = map[string]interface{}{
+					"Name":       project.Name,
+					"Project ID": project.Id,
+					"Org ID":     project.OrgId,
+				}
+			}
+
+			if common.OutputFormat == "json" {
+				printer.PrintResource(utils.FormatOutput(data, common.OutputFormat))
+				return nil
+			}
+
+			response := charm.SuccessStyle.Render(utils.FormatOutput(data, common.OutputFormat))
+			printer.PrintResource(response)
+			return nil
+
+		},
+	}
+	cmd.Flags().StringVarP(&name, "name", "n", "", "name of the project")
+	_ = cmd.MarkFlagRequired("name")
+	return cmd
+}
